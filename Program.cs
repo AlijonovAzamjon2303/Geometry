84007e9e163d5526c522adb234d47d2dd24cf77a
A'zamjon <alijonovrobo@gmail.com>
2024-02-12 21:05:44 +0500

    First Upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("1. Doira yuzini aniqlash");
+Console.WriteLine("1. Silindr hajmini aniqlash\n");
+Console.Write("Tanlovingizni kiriting : ");
+string input = Console.ReadLine();
+switch(input)
+{
+    case "1" : 
+        {
+            Console.Write("R = ");
+            string strRadius = Console.ReadLine();
+            double radius = Convert.ToDouble(strRadius);
+            Console.WriteLine($"R = {strRadius} bo'lsa S = {Math.PI * radius * radius}");
+        }
+        break;
+    case "2" :
+        {
+            Console.Write("R = ");
+            string strRadius = Console.ReadLine();
+            Console.Write("H = ");
+            string strHigh = Console.ReadLine();
+            double radius = Convert.ToDouble(strRadius);
+            double high = Convert.ToDouble(strHigh);
+            Console.WriteLine($"Silindrning hajmi {Math.PI * radius * radius * high}");
+        }
+        break;
+}
